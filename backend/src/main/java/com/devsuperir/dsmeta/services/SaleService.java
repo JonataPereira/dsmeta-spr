b5ba8b7d029b96038abeea17bccfa04abfa17b22
Jonata Pereira <jonatap.pessoal@gmail.com>
2022-07-17 17:00:45 -0300

    Api Teste

---
+++ backend/src/main/java/com/devsuperir/dsmeta/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.devsuperir.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperir.dsmeta.entities.Sale;
+import com.devsuperir.dsmeta.repositories.SaleRepository;
+
+
+@Service
+public class SaleService {
+	
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+
+}
